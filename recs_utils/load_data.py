00e2b5b98d91de94aa2a5349c477686cd1ab849d
KernelA <17554646+KernelA@users.noreply.github.com>
2023-05-28 13:07:14 +0300

    Add numba sppedup to compute pairwise distances and matrix fatcorization example

--- recs_utils/load_data.py
+++ recs_utils/load_data.py
@@ -1,9 +1,14 @@
+from typing import List
+
 import polars as pl
 
 from .metrics import USER_ID_COL, ITEM_ID_COL
 
 
 class MovieLens100K:
+    """https://www.kaggle.com/datasets/prajitdatta/movielens-100k-dataset
+    """
+
     ITEM_COLUMNS = [
         ITEM_ID_COL, 'title', 'release date', 'video release date',
         'IMDb URL ', 'unknown', 'Action', 'Adventure', 'Animation',
@@ -13,35 +18,86 @@ class MovieLens100K:
     ]
 
     @staticmethod
-    def load(path_to_date: str, path_to_items: str, item_num_read_columns: int = 2):
+    def load_items(path_to_data: str, item_columns: List[str] = [ITEM_ID_COL, "title"]):
+        column_indices = [MovieLens100K.ITEM_COLUMNS.index(col) for col in item_columns]
+
+        return pl.read_csv(
+            path_to_data,
+            separator='|',
+            has_header=False,
+            encoding="latin-1",
+            dtypes={ITEM_ID_COL: pl.UInt32},
+            columns=column_indices,
+            new_columns=item_columns
+        )
+
+    @staticmethod
+    def load_interactions(path_to_data: str, sort: bool = True):
         ratings = pl.read_csv(
-            path_to_date,
+            path_to_data,
             separator='\t',
             has_header=False,
             columns=[0, 1, 2],
             new_columns=[USER_ID_COL, ITEM_ID_COL, "rating"],
             dtypes=[pl.UInt32, pl.UInt32, pl.Float32]
-        ).sort([USER_ID_COL, ITEM_ID_COL])
-
-        item_num_read_columns = min(len(MovieLens100K.ITEM_COLUMNS), item_num_read_columns)
-
-        movies = pl.read_csv(
-            path_to_items,
-            separator='|',
-            has_header=False,
-            encoding="latin-1",
-            dtypes={ITEM_ID_COL: pl.UInt32},
-            columns=list(range(item_num_read_columns)),
-            new_columns=MovieLens100K.ITEM_COLUMNS[:item_num_read_columns],
         )
 
-        return ratings, movies
+        if sort:
+            ratings = ratings.sort([USER_ID_COL, ITEM_ID_COL])
+
+        return ratings
 
 
-def load_users(path_to_file: str):
-    df_users = pl.read_csv(path_to_file, dtypes={
-        "age": pl.Categorical, USER_ID_COL: pl.UInt32, "sex": pl.Float32})
-    return df_users
+class MTSDataset:
+    """https://www.kaggle.com/datasets/sharthz23/mts-library
+    """
+
+    @staticmethod
+    def load_interactions(path_to_data: str):
+        interactions = pl.read_csv(path_to_data,
+                                   dtypes={
+                                       USER_ID_COL: pl.UInt32,
+                                       ITEM_ID_COL: pl.UInt32,
+                                       "start_date": pl.Date,
+                                       "rating": pl.Float32,
+                                       "progress": pl.UInt8
+                                   }).with_columns(pl.col("rating").fill_null(float("nan")))
+
+        uniq_items = interactions.select(
+            pl.col(USER_ID_COL),
+            pl.col(ITEM_ID_COL)
+        )
+
+        not_duplicated_interactions = interactions.filter(uniq_items.is_unique())
+        duplicated_interactions = interactions.filter(uniq_items.is_duplicated()).groupby([USER_ID_COL, ITEM_ID_COL]).agg(
+            [
+                pl.max("progress"),
+                pl.max("rating"),
+                pl.min("start_date")
+            ]
+        )
+
+        return not_duplicated_interactions.vstack(duplicated_interactions)
+
+    @staticmethod
+    def load_items(path_to_data: str):
+        return pl.read_csv(
+            path_to_data,
+            dtypes={
+                "id": pl.UInt32,
+                "genres": pl.Categorical,
+                "authors": pl.Categorical,
+                "year": pl.Categorical
+            },
+            new_columns=[ITEM_ID_COL]
+        )
+
+    @staticmethod
+    def load_users(path_to_data: str):
+        return pl.read_csv(path_to_data, dtypes={
+            "age": pl.Categorical,
+            USER_ID_COL: pl.UInt32,
+            "sex": pl.Float32})
 
 
 def sample_true_rec_data():
@@ -70,43 +126,3 @@ def sample_true_rec_data():
     }
     )
     return df_true, df_recs
-
-
-def load_interactions(path_to_file: str):
-    interactions = pl.read_csv(path_to_file,
-                               dtypes={
-                                   USER_ID_COL: pl.UInt32,
-                                   ITEM_ID_COL: pl.UInt32,
-                                   "start_date": pl.Date,
-                                   "rating": pl.Float32,
-                                   "progress": pl.UInt8
-                               }).with_columns(pl.col("rating").fill_null(float("nan")))
-
-    uniq_items = interactions.select(
-        pl.col(USER_ID_COL),
-        pl.col(ITEM_ID_COL)
-    )
-
-    not_duplicated_interactions = interactions.filter(uniq_items.is_unique())
-    duplicated_interactions = interactions.filter(uniq_items.is_duplicated()).groupby([USER_ID_COL, ITEM_ID_COL]).agg(
-        [
-            pl.max("progress"),
-            pl.max("rating"),
-            pl.min("start_date")
-        ]
-    )
-
-    return not_duplicated_interactions.vstack(duplicated_interactions)
-
-
-def load_items(path_to_file: str):
-    return pl.read_csv(
-        path_to_file,
-        dtypes={
-            "id": pl.UInt32,
-            "genres": pl.Categorical,
-            "authors": pl.Categorical,
-            "year": pl.Categorical
-        },
-        new_columns=[ITEM_ID_COL]
-    )
